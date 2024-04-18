61f61ba4bd02671d30dc6db459e0df9981a617a5
Banee Ishaque K <baneeishaque@gmail.com>
2024-04-18 10:17:53 +0530

    VS 2022 Project Start

---
+++ C-Parser-CPP-ANTLR-Visual-Studio-2022/C-Parser-CPP-ANTLR-Visual-Studio-2022.cpp
@@ -0,0 +1,12 @@
+﻿// C-Parser-CPP-ANTLR-Visual-Studio-2022.cpp : Defines the entry point for the application.
+//
+
+#include "C-Parser-CPP-ANTLR-Visual-Studio-2022.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
